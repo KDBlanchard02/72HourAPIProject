40bf10b79f4e6354ade946a5b010383c5bdee8f7
Kevin Blanchard <kevindanielblanchard@gmail.com>
2021-08-10 21:06:47 -0400

    Add project files.

---
+++ 72HourAPIProject/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace _72HourAPIProject.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ 72HourAPIProject/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(_72HourAPIProject.Startup))]
+
+namespace _72HourAPIProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
