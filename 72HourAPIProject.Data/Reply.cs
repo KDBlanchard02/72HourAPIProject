7c6b549fd9576da142dd7278ace9c107a3723418
Kevin Blanchard <kevindanielblanchard@gmail.com>
2021-08-12 18:22:54 -0400

    Added reply class

---
+++ 72HourAPIProject.Data/Reply.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeventyTwoHourAPIProject.Data
+{
+    public class Reply
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey(nameof(Comment))]
+        public int CommendId { get; set; }
+
+        [Required]
+        public Guid AuthorId { get; set; }
+        [Required]
+        public string Text { get; set; }
+
+        public virtual Comment comment { get; set; }
+
+    }
+}
